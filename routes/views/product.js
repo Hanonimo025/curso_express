a3c1b7b7eac34bb14aa5e3e0bd162c8fd965a0b8
Victor Ruiz <vik_ases@live.com.mx>
2019-07-19 00:40:44 -0500

    Prueba de autorizacion en rutas

--- routes/views/product.js
+++ routes/views/product.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const ProductService = require('../../services/product');
 
 const productService = new ProductService();
+
+//jwt
+require('../../utils/auth/strategies/jwt');
 
 router.get('/', async (req, res, next) => {
     const {
@@ -18,6 +22,10 @@ router.get('/', async (req, res, next) => {
     } catch (err) {
         next(err)
     }
+});
+
+router.get('/prueba', passport.authenticate('jwt', {sesion: false}), (req, res, next) => {
+    res.render('prueba');
 });
 
 module.exports = router;
